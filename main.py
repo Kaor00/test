cdc6e3e80944118d63d1350d4dbc36086d1c03cb
Kaor00 <sat.poisen@gmail.com>
2025-06-14 12:30:13 +0300

    Create file main

---
+++ main.py
@@ -0,0 +1,16 @@
+class Gamer:
+    __count = 0
+    def __init__(self, name):
+        self.name = name
+        Gamer.__count += 1
+
+    def print(self):
+        print(f"Gamer: {self.name}")
+
+    @staticmethod
+    def amount():
+        return Gamer.__count
+
+gamer1 = Gamer(input("Enter your name: "))
+gamer2 = Gamer(input("Enter your name: "))
+print('Game beginner!' if Gamer.amount()==2 else "await second gamer...")
